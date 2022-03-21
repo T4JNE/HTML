cc9d15d38bbc2825ff6ccb47d26d7e7607551931
Bob bobb <thnovtntjmmbdquipw@niwghx.online>
2022-03-21 18:11:52 +0100

    JQuery test

--- js/form.js
+++ js/form.js
@@ -1,3 +1,9 @@
 function divClick(){
   document.getElementById("demo").innerHTML = "clickedElement";
 }
+
+var x1 = 0;
+
+$(".radio-btn").click(function(){
+  $(this).toggleClass("div-selected");
+});
