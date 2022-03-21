e843b6564e6402ffeab00340cce22fcb9141e206
Bob bobb <thnovtntjmmbdquipw@niwghx.online>
2022-03-21 18:41:21 +0100

    Zmienony formularz, teraz działcza

--- js/form.js
+++ js/form.js
@@ -1,9 +1,3 @@
-function divClick(){
-  document.getElementById("demo").innerHTML = "clickedElement";
-}
-
-var x1 = 0;
-
-$(".radio-btn").click(function(){
-  $(this).toggleClass("div-selected");
-});
+//$(".radio-btn").click(function(){
+//  $(this).toggleClass("div-selected");
+//});
