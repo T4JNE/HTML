f694f32e2e675ee09680620d5023fe14326da53f
Bob bobb <thnovtntjmmbdquipw@niwghx.online>
2022-03-21 19:07:58 +0100

    Animacja menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 function MenuClick(){
+  //document.getElementById("DropMenu").classList.toggle("dropdown-content-show");
   document.getElementById("DropMenu").classList.toggle("dropdown-content-show");
 }
